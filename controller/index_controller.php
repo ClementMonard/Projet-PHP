048198c95bacb2e025ad78cc230651f69feccb5f
ClementMonard <clement.monard@hotmail.fr>
2018-09-10 09:03:46 +0200

    Ajout de commentaires

--- controller/index_controller.php
+++ controller/index_controller.php
@@ -1,17 +1,23 @@
 <?php
 
+//Si le fichier existe
 if (file_exists('source.xml')) {
+    //Alors tu charges la page
     $xml = simplexml_load_file('source.xml');
 } else {
+    //Sinon message d'erreur
     exit('Echec');
 }
 
+//Contrôle que la variable page existe
 if (isset($_GET['page'])){
+    //Retourne la valeur numérique entière équivalente d'une variable avec la fonction intval avec une sécurité supplémentaire en utilisant la fonction htmlspecialchars
     $page = intval(htmlspecialchars($_GET['page']));
 } else {
     $page = 0;
 }
 
+//Création d'une variable $title qui aura comme contenu la valeur page et le titre de la page xml
  $title = $xml->page[$page]->title;
  
 ?>
--- index.php
+++ index.php
@@ -6,6 +6,7 @@ include 'controller/index_controller.php';
     <head>
         <meta charset = "utf-8" />
         <link rel="stylesheet" href="style.css" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <title><?= $title ?></title>
     </head>
     <body>
