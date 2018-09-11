9f9d0a8387ab830faee85fdf764b40e5ec97b243
ClementMonard <clement.monard@hotmail.fr>
2018-09-11 08:36:41 +0200

    Modification du .htaccess et ajout d'une erreur 404

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ include 'controller/index_controller.php';
 <html lang="fr" dir="ltr">
     <head>
         <meta charset = "utf-8" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" crossorigin="anonymous">  
         <link rel="stylesheet" href="style.css" />
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" crossorigin="anonymous">        
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" crossorigin="anonymous"></script>
@@ -20,8 +20,8 @@ include 'controller/index_controller.php';
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <?php for ($i = 0; $i < count($test); $i++) { ?></li>
-                        <li><a href="?page=<?= $i; ?>"><?= $xml->page[$i]->menu ?></a></li>
+                        <?php for ($countPages = 0; $countPages < count($displayPages); $countPages++) { ?></li>
+                        <li><a href="/page-<?= $countPages; ?>.html"><?= $xml->page[$countPages]->menu ?></a></li>
                         <li><?php } ?></li>
                 </ul>
             </div>
